01f704561c7e6ae069a175f34b80cf246deaac74
notebook <kkang56508@naver.com>
2024-09-04 23:26:15 +0900

    aws 세팅

--- src/main/java/com/forest/ForestApplication.java
+++ src/main/java/com/forest/ForestApplication.java
@@ -2,13 +2,14 @@ package com.forest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-public class ForestApplication {
+public class ForestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForestApplication.class, args);
